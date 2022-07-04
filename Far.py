2c0a6fa8e92380a4544227e14e1ef94eca4c8eae
Hecea <81994410+Hecea@users.noreply.github.com>
2022-07-04 23:59:27 +0200

    Add files via upload

---
+++ Far.py
@@ -0,0 +1,6 @@
+#petit questionnaire nominatif sur le far Breton
+nom = input("quel est votre nom ? ")
+age = input ("quel est votre age ? ")
+gout = input("pourquoi aimez-vous le far Breton ? ")
+print ("Vous vous appelez " + nom + ",vous avez " + str(age) + " ans" " et vous aimez le far Breton : " + gout ) 
+# concatenation +
