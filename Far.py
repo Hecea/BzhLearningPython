8b0d0d3a58b72e4eef62e1fa834303d7080d87a9
Hecea <81994410+Hecea@users.noreply.github.com>
2022-07-05 00:04:43 +0200

    Update Far.py

--- Far.py
+++ Far.py
@@ -2,5 +2,5 @@
 nom = input("quel est votre nom ? ")
 age = input ("quel est votre age ? ")
 gout = input("pourquoi aimez-vous le far Breton ? ")
-print ("Vous vous appelez " + nom + ",vous avez " + str(age) + " ans" " et vous aimez le far Breton : " + gout ) 
-# concatenation +
+# print ("Vous vous appelez " + nom + ",vous avez " + str(age) + " ans" " et vous aimez le far Breton : " + gout ) 
+# concatenation 
