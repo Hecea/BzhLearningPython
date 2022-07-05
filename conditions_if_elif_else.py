968bd62938100841224dafff95e60084ff75b0d8
Hecea <81994410+Hecea@users.noreply.github.com>
2022-07-05 23:48:42 +0200

    Add files via upload
    conditions if elif else

---
+++ conditions_if_elif_else.py
@@ -0,0 +1,14 @@
+# rappel commentaire sur une ligne
+""" commentaires sur plusieurs lignes
+aussi pour mettre du code en attente
+une ligne ou un bloc """
+
+a = 5
+b = 56
+
+if b > a: # si True s'affichera :
+    print("b est plus grand que a")
+elif a==b:
+    print("a et b sont égaux") # condition False ne s'affiche pas
+else :
+    print("a est plus grand que b")
