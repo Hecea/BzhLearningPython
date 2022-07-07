bcde9b1a8685306a9f1dc07a5695678e8add780a
Hecea <81994410+Hecea@users.noreply.github.com>
2022-07-07 23:50:53 +0200

    Add files via upload
    types variables : string, integer, float, boolean

---
+++ types.py
@@ -0,0 +1,6 @@
+# Types de variables
+a = "chaine de caractere" # str string
+b = 7 # int integer nombre entier
+c = 7.9 # float nombre a virgule flottante
+d = True # True or Fasle Boolean valeur booleenne
+
