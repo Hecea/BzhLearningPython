8657884a92800cf55a3ba4b32a475309379c10e1
Hecea <81994410+Hecea@users.noreply.github.com>
2022-07-08 23:59:27 +0200

    Add files via upload
    changement valeur d'un elememt de liste

--- liste.py
+++ liste.py
@@ -1,4 +1,6 @@
 # list [element0,element1,element2,elementn]
 #indice start at zero
 temperatures = [10,15,36,7]
+temperatures[3]=27 # change element 
 print(temperatures[1])
+print(temperatures[3])
