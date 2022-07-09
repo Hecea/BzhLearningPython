544f1a9d3350d2397b7bef349eb709658bd37b62
Hecea <81994410+Hecea@users.noreply.github.com>
2022-07-09 23:50:06 +0200

    Add files via upload
    .pop

--- liste.py
+++ liste.py
@@ -1,7 +1,7 @@
-# list [element0,element1,element2,elementn]
-#indice start at zero
 temperatures = [10,15,36,7]
-temperatures[3]=27 # change element 
-temperatures[2]=25
+temperatures[3]=27 # change element
+print(temperatures)
 print(temperatures[1])
 print(temperatures[3])
+temperatures.pop(1)
+print(temperatures)
