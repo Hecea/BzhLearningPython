119ccf6c816a25f8f2b7e1e6abcf0af819453bd9
Hecea <81994410+Hecea@users.noreply.github.com>
2022-07-12 23:45:32 +0200

    Add files via upload
    update variable

--- replace.py
+++ replace.py
@@ -3,3 +3,6 @@ saison = "Aujourd'hui c'est l'été"
 new_saison = saison.replace("l'été","le printemps")
 print(new_saison)
 print(saison)
+saison = new_saison # re affection de la variable
+print(saison)
+
