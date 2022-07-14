531a44bb53b74134b0b31b72f56307c8c3f6d7af
Hecea <81994410+Hecea@users.noreply.github.com>
2022-07-14 23:54:51 +0200

    Add files via upload
    Ajout exemple en commentaire

--- if.py
+++ if.py
@@ -5,3 +5,15 @@ partie = nb_lancer == 7
 
 if partie :
     print("la partie est terminee")
+
+'''
+ # if avec nouvelle affectation de variable
+
+ fruit = "cerises"
+ preparation = "denoyautees"
+
+ if fruit = "cerises"
+     preparation = "non-denoyautees"
+
+print(preparation)
+ '''
