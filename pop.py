dd342bd200228f85052c9ab1694a57ddce6eb8ff
Hecea <81994410+Hecea@users.noreply.github.com>
2022-07-16 08:27:38 +0200

    Add files via upload
    .insert( ) pour inserer un element (indice,element)

--- pop.py
+++ pop.py
@@ -1,10 +1,18 @@
 # pop () sans position specifiee renvoie au dernier argument de la liste
-
 mot_de_passe = ["Tracteur","charrue", "Semoir", "Moissonneuse-batteuse", "Herse"]
 code = mot_de_passe.pop()
 print(code)
+
+# .append( ) ajouter un element
 mot_de_passe.append(9)
 print(mot_de_passe)
 print(code)
 code = mot_de_passe.pop()
 print(code)
+
+# .insert( ) insert un element
+mot_de_passe.insert(3,"abreuvoir")
+print(mot_de_passe)
+
+
+
