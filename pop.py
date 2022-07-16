20cacf39c21d4a9e82158466c43ada56e74250dc
Hecea <81994410+Hecea@users.noreply.github.com>
2022-07-16 07:54:45 +0200

    Add files via upload
    pop( ) sans position specifiee renvoie au dernier element de la liste

---
+++ pop.py
@@ -0,0 +1,7 @@
+# pop () sans position specifiee renvoie au dernier argument de la liste
+
+mot_de_passe = ["Tracteur","charrue", "Semoir", "Moissonneuse-batteuse", "Herse"]
+code = mot_de_passe.pop()
+print(code)
+
+
