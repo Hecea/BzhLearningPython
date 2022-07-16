14e90494ffaf3deea248b70a58fa0705fa6974ec
Hecea <81994410+Hecea@users.noreply.github.com>
2022-07-16 23:56:25 +0200

    Add files via upload
    affectation de len( ) à une variable

--- len.py
+++ len.py
@@ -1,3 +1,5 @@
 # len( ) pour avoir le nombre d elements d'une liste
 bouchees = ["praliné noir", "praliné lait"]
 print(len(bouchees))
+# len( ) affecte à une variable
+nb_choix_bouchees = len(bouchees)
