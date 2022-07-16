5cc229a5023d4b4070beafcae2adc201788988b9
Hecea <81994410+Hecea@users.noreply.github.com>
2022-07-16 08:02:30 +0200

    Add files via upload
    .append( ) ajout

--- pop.py
+++ pop.py
@@ -3,5 +3,8 @@
 mot_de_passe = ["Tracteur","charrue", "Semoir", "Moissonneuse-batteuse", "Herse"]
 code = mot_de_passe.pop()
 print(code)
-
-
+mot_de_passe.append(9)
+print(mot_de_passe)
+print(code)
+code = mot_de_passe.pop()
+print(code)
