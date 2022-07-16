f42a4fb97a1e935013d700e0be78cada0734e68c
Hecea <81994410+Hecea@users.noreply.github.com>
2022-07-17 00:42:45 +0200

    Add files via upload
    for ... in + 1

--- forLoop.py
+++ forLoop.py
@@ -1,4 +1,9 @@
 # for ... in
+
 chocolats = ["lait", "noir", "blanc"]
 for chocolat in chocolats :
     print(chocolat)
+
+    chocolats = [1, 2, 3]
+    for chocolat in chocolats:
+         print(chocolat + 1)
