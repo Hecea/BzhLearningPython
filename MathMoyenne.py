d35955277890b190e01e0ba1ded29332ee6609c7
Hecea <81994410+Hecea@users.noreply.github.com>
2022-07-18 23:57:14 +0200

    Add files via upload
    moyenne n nombres  for in range( )

--- MathMoyenne.py
+++ MathMoyenne.py
@@ -1,6 +1,18 @@
 # moyenne de 2 nombres
-
+"""
 a = float(input("entrez a : "))
 b = float(input("entrez b : "))
 moyenne = (a + b )/2
 print("La moyenne de a et b est :", moyenne)
+"""
+# calcul moyenne de n nombres
+
+n = int(input("entrez la valeur de n : "))
+moyenneN = 0
+
+for a in range(1, n+1) :
+   x = float(input("entrez x : "))
+   moyenneN = moyenneN + x
+   print (moyenneN/n)
+
+
