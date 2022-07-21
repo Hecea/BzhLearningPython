44c1261abdc2647249de702d225d4dbe1bb0cfa9
Hecea <81994410+Hecea@users.noreply.github.com>
2022-07-21 23:57:34 +0200

    Add files via upload
    complement

--- fString.py
+++ fString.py
@@ -3,3 +3,10 @@
 vide = 88
 
 print(f"You have {vide}  cartons vides ")
+
+livraison  = 11
+vide = vide + livraison
+print(f" Vous avez recus  {livraison} cartons vides ")
+print(f" Vous avez {vide}  cartons vides")
+
+z
