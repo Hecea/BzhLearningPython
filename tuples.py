3fcb96fca31c089a39d54f42c6478b192e286778
Hecea <81994410+Hecea@users.noreply.github.com>
2022-07-24 01:00:11 +0200

    Add files via upload
    tuple type complexe,  liste non mutable

---
+++ tuples.py
@@ -0,0 +1,9 @@
+# tuple c' est liste entre parenthese non modifiable
+# on dit qu'il n'est pas mutable, type complexe
+
+# on accede aux elemnts par les indices
+
+#  for  ... in pour parcourir le tuple
+
+livres_sur_letagere = ('Hugo" , " Moliere" , "Zola")
+                       
