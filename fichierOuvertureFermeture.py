d9bd396076052b1ae3390bc1cac4ccc84f8b2ac8
Hecea <81994410+Hecea@users.noreply.github.com>
2022-07-23 23:59:11 +0200

    Add files via upload
    explication mode 
    + b pour binaire

--- fichierOuvertureFermeture.py
+++ fichierOuvertureFermeture.py
@@ -1,6 +1,14 @@
 # open( )
 # close( ) TOUJOURS FERME UN FICHIER SINON IL NE SERA PAS ACCESSIBLE
 
-nom_fichier = open("fichier.txt", "r")
+nom_fichier = open("fichier.txt", "w")
+''' 
+modes : 
+r pour read 
+w pour write : dans ce cas le contenu du fichier est ecrase ou 
+                le fichier est cree si il n'existe pas.
+a pour ouverture en ecriture en mode ajout  
+            le fichier est cree si besoin '''
+# b pour binaire
 
 nom_fichier.close()
