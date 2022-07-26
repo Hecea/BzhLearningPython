d8e15be4fbd5c7c9b3d545361cf461cfb751764a
Hecea <81994410+Hecea@users.noreply.github.com>
2022-07-26 23:32:21 +0200

    Add files via upload
    if elif else

---
+++ conduire.py
@@ -0,0 +1,10 @@
+""""# condion if  elif  else
+direction = "a droite"
+
+if direction == "a droite"
+    print("Tourner a droite")
+    elif direction == 'a gauche'
+    print("Tourner a gauche")
+else :
+    print("Aller tout droit")
+"""
