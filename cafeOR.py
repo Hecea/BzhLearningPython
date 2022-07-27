d7ba23044635d91810c173db73ec76afcf5bbab5
Hecea <81994410+Hecea@users.noreply.github.com>
2022-07-27 08:42:01 +0200

    Add files via upload
    if ... or ... : else:

---
+++ cafeOR.py
@@ -0,0 +1,9 @@
+# if ... or ... : else:
+
+cafe = True
+time = "De jour"
+
+if cafe == False or time != "Nuit":
+    print("Nul besoin de compter les etoiles pour s'endormir")
+else:
+    print("Tu ne dormiras pas de la nuit")
