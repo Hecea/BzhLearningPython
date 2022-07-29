152b80bbbcab9c29346a8109ac32185f4a18a032
Hecea <81994410+Hecea@users.noreply.github.com>
2022-07-29 23:58:29 +0200

    Add files via upload

---
+++ keywordDEF.py
@@ -0,0 +1,14 @@
+# def
+def compter_les_allures_naturelles(allures):
+    print(len(allures))
+    allures = ["trot",'"galop","pas","reculer"]
+    print(allures)
+
+
+
+
+"""
+def nombre_de_jours_dans_annee():
+    print("365")
+    nombre_de_jours_dans_annee()
+"""
