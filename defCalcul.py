5cfd5e6b509d3297d32f7613d539e32387dc999b
Hecea <81994410+Hecea@users.noreply.github.com>
2022-07-31 19:39:56 +0200

    Add files via upload
    def fonction (parametre) :

--- defCalcul.py
+++ defCalcul.py
@@ -1,6 +1,13 @@
 # definition de fonction  exemple avec calcul
 
+def ajouter_trois_a_ParametreN (ParametreN) :
+    print(ParametreN + 9)
+
+ajouter_trois_a_ParametreN (21)
+
+
 def ajouter_trois_a_variableN (variableN) :
     print(variableN + 3)
 
 ajouter_trois_a_variableN (21)
+
