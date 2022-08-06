898a1a417212dbeb46c148f1dbe8c5a203d97b95
Hecea <81994410+Hecea@users.noreply.github.com>
2022-08-07 00:00:11 +0200

    Add files via upload

--- return.py
+++ return.py
@@ -1,4 +1,16 @@
-# keyword return
-def ajouterTrois():
-    return 3
-print(ajouterTrois())
+# return mot-cle stop la fonction courante Renvoyer la valeur
+"""
+x = 0
+y= 0
+z = 0
+
+def difference(x,y):
+    return x-y
+
+Z = difference(5,3)
+
+    print(x)
+    print(y)
+    print(z)
+
+"""
