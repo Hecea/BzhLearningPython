41df5a030118f6c8f2fbdd425aed3bef33485e54
Hecea <81994410+Hecea@users.noreply.github.com>
2022-08-07 10:01:29 +0200

    Add files via upload

---
+++ dictionnaireMineraux.py
@@ -0,0 +1,9 @@
+# Un dictionnaire est un ensemble d elements
+# le dictionnaire = {     }
+# 1 element = 1 cle : 1 valeur
+
+metalAlcalin = {"Li":"Lithium","Na" : "Sodium"}
+metalAlcalin["Li"]
+print(metalAlcalin)
+Li = metalAlcalin["Li"]
+print(Li)
