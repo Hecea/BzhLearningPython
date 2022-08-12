6a3f083a97b7627fc07410ea71708cb001a8205d
Hecea <81994410+Hecea@users.noreply.github.com>
2022-08-13 00:07:22 +0200

    Add files via upload
    conversion noeud/kilometre

--- miles.py
+++ miles.py
@@ -1,6 +1,14 @@
 # conversion miles kilometres
-miles = 200
+# 1 miles equivaut a 1.609
+
+miles = 1
 kilometres = miles * 1.609
 print(kilometres)
 
-# faire conversion noeud vitesse vent 
+# faire conversion noeud vitesse vent
+# 1 noeud equivaut a 1.85 km/h
+
+noeud = 1
+kilometreNoeud = noeud * 1.85
+print(kilometreNoeud)
+
