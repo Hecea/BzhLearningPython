7d0635e35b565f7c980275038d9b6ca5e213ccd8
Hecea <81994410+Hecea@users.noreply.github.com>
2022-09-13 11:36:33 +0200

    Add files via upload
    OOP creation class

---
+++ OopEnPooFr.py
@@ -0,0 +1,17 @@
+"""Programmation Orientee Objet langage python
+Encapsulation des donnees et des fonctions
+creation d une classe instruction class
+
+class NomdemaclasseMineraux:
+    def definition_attribut_instance(self,nom,couleur,gisement):
+        """
+
+# creation de la class Pratiquer ... un sport
+class Pratiquer:
+    sport = "roller"
+    nom = sport
+    frequence = "quotidienne"
+    duree = 45
+    print(sport, frequence, duree)
+
+    # def faire_sport(self, nom, frequence, duree):
