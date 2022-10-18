317c72589c15b6084731f09258f7cd021cdc8782
Hecea <81994410+Hecea@users.noreply.github.com>
2022-10-18 23:36:04 +0200

    Add files via upload
    input if else

---
+++ inputFileVoileOuVelo.py
@@ -0,0 +1,9 @@
+# execution ds le terminal : version python + nom_fichier.py
+entree_minutes_disponibles = input ('Avez-vous une journee de disponible ?  (repondre par oui ou non) ')
+if entree_minutes_disponibles == 'oui':
+    print("Vous avez le temps pour une activite nautique")
+else:
+    print("Vous avez le temps pour une sortie a velo")
+
+
+
